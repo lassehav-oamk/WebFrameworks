039c0edaa29f526b96a0d08f36b6b2dc6fa9e3c1
Lasse Haverinen <lasse.haverinen@gmail.com>
2019-08-13 09:35:23 +0300

    jsprimer updated with try catch to prevent demo from crashing

--- react-intro/0_jsPrimer.js
+++ react-intro/0_jsPrimer.js
@@ -8,18 +8,22 @@ function varletloop () {
     console.log('let loop with a value ' + a);
   }
 
-  console.log('value of a after loop ' + a);
+  try {
+    console.log('value of a after loop ' + a);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-//varletloop();
+varletloop();
 
 var x = 1;
-function a () {
+function example () {
   y = 2; // y is set to global scope without
-  var z = 3;
+  z = 3;
 }
 
-a();
+example();
 console.log('variable x value ' + x);
 console.log('variable y value ' + y);
 console.log('variable z value ' + z);
