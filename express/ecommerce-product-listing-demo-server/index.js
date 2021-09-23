9f9dc4501ecd5b47e58b332c73278daeb0527e02
Lasse Haverinen <lasse.haverinen@gmail.com>
2021-09-23 12:03:56 +0300

    Added create and delete options to ecommerce demo server

--- express/ecommerce-product-listing-demo-server/index.js
+++ express/ecommerce-product-listing-demo-server/index.js
@@ -3,8 +3,16 @@ const app = express()
 const port = 4000
 const data = require('./data.json');
 const cors = require('cors')
+const { v4: uuidv4 } = require('uuid');
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
 app.use(cors());
+
+// reset ID values of every item from data.json
+for(let i = 0; i < data.items.length; i++) {
+  data.items[i].id = uuidv4();
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -13,6 +21,31 @@ app.get('/', (req, res) => {
 app.get('/products', (req, res) => {
   res.json(data.items);
 });
+
+app.post('/products', (req, res) => {
+  data.items.push({
+      "id": uuidv4(),
+      "name": req.body.name,
+      "author": req.body.author,
+      "type": "Paperback",
+      "price": req.body.price,
+      "rating": req.body.rating,
+      "promos": [
+      ]
+    });
+  res.sendStatus(201);
+});
+
+app.delete('/products/:id', (req, res) => {
+  const productIndex = data.items.findIndex(item => item.id === req.params.id);
+  if(productIndex != -1) {
+    data.items.splice(productIndex, 1);
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(404);
+  }
+
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
